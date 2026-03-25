3fdd5c62ecde4885a663b560daf0545799b2e558
Yahir Orozco <yorozcolop49@gmail.com>
2026-03-25 16:36:58 -0700

    added return_book_with_id and changed add_book post function

--- src/api/book_api.py
+++ src/api/book_api.py
@@ -1,5 +1,5 @@
-from models.book import Book
-from services.book_service import (return_all_books, add_book)
+from models.book import Book, BookWithID
+from services.book_service import (return_all_books, add_book, return_book_with_id)
 from fastapi import APIRouter, HTTPException
 
 # create crud functions for the books that return json
@@ -7,15 +7,17 @@ from fastapi import APIRouter, HTTPException
 book_api = APIRouter()
 
 @book_api.get("/api/books")
-async def all_books():
+async def all_books() -> list[BookWithID]:
     books = return_all_books()
     return books
-    
 
+@book_api.get("/api/book/{book_id}")
+async def book(book_id: str) -> BookWithID:
+    return return_book_with_id(book_id)
+    
 # adds book to db
 @book_api.post("/api/book")
-async def book(book: Book) -> Book:
-    add_book(book)
-    return book
+async def book(book: Book) -> BookWithID:
+    return add_book(book)
 
 
--- src/services/book_service.py
+++ src/services/book_service.py
@@ -4,16 +4,21 @@ from bson import ObjectId
 
 # used to run queries on db and pass to other modules
 
-def return_all_books() -> list[BookWithID] :
+def return_all_books() -> list[Book] :
     books = []
     for book in book_coll.find():
         book["id"] = str(book["_id"])
         del book["_id"]
         books.append(BookWithID(**book))
-    # books = [BookWithID(**book) for book in book_coll.find()]
-    print(books)
     return books
 
+def return_book_with_id(book_id: str) -> BookWithID:
+    book_response = book_coll.find_one({"_id": ObjectId(book_id)})
+    book_response["id"] = str(book_response["_id"])
+    del book_response["_id"]
+    return BookWithID(**book_response)
+
 def add_book(book: Book):
-    book_response = book_coll.insert_one(book.model_dump(exclude_none=True))
+    result = book_coll.insert_one(book.model_dump(exclude_none=True))
+    book_response = BookWithID(id=str(result.inserted_id), **book.model_dump())
     return book_response
