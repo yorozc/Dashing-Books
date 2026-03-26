6f4d9b4de62dac5e823f601f8bab9f97924637ca
Yahir Orozco <yorozcolop49@gmail.com>
2026-03-25 18:13:14 -0700

    created put api route

--- src/api/book_api.py
+++ src/api/book_api.py
@@ -1,6 +1,6 @@
 from models.book import Book, BookWithID
-from services.book_service import (return_all_books, add_book, return_book_with_id)
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
+from services.book_service import BookService
 
 # create crud functions for the books that return json
 
@@ -8,16 +8,24 @@ book_api = APIRouter()
 
 @book_api.get("/api/books")
 async def all_books() -> list[BookWithID]:
-    books = return_all_books()
+    books = BookService.return_all_books()
     return books
 
 @book_api.get("/api/book/{book_id}")
 async def book(book_id: str) -> BookWithID:
-    return return_book_with_id(book_id)
+    return BookService.return_book_with_id(book_id)
     
 # adds book to db
 @book_api.post("/api/book")
 async def book(book: Book) -> BookWithID:
-    return add_book(book)
+    return BookService.add_book(book)
+
+@book_api.put("/api/book/{book_id}")
+async def book(book_id: str, book: Book) -> Book:
+    return BookService.edit_book(book_id, book)
+
+@book_api.delete("api/book/{book_id}")
+async def book(book_id: str):
+    return BookService.delete_book(book_id)
 
 
--- src/services/book_service.py
+++ src/services/book_service.py
@@ -1,24 +1,42 @@
 from database.db import book_coll
 from models.book import Book, BookWithID
 from bson import ObjectId
+from fastapi import HTTPException
 
 # used to run queries on db and pass to other modules
 
-def return_all_books() -> list[Book] :
-    books = []
-    for book in book_coll.find():
-        book["id"] = str(book["_id"])
-        del book["_id"]
-        books.append(BookWithID(**book))
-    return books
+# add error handling
+class BookService:
+    def __init__():
+        pass
 
-def return_book_with_id(book_id: str) -> BookWithID:
-    book_response = book_coll.find_one({"_id": ObjectId(book_id)})
-    book_response["id"] = str(book_response["_id"])
-    del book_response["_id"]
-    return BookWithID(**book_response)
+    def return_all_books() -> list[Book]:
+        books = []
+        for book in book_coll.find():
+            book["id"] = str(book["_id"])
+            del book["_id"]
+            books.append(BookWithID(**book))
+        return books
 
-def add_book(book: Book):
-    result = book_coll.insert_one(book.model_dump(exclude_none=True))
-    book_response = BookWithID(id=str(result.inserted_id), **book.model_dump())
-    return book_response
+    def return_book_with_id(book_id: str) -> BookWithID:
+        book_response = book_coll.find_one({"_id": ObjectId(book_id)})
+        book_response["id"] = str(book_response["_id"])
+        del book_response["_id"]
+        return BookWithID(**book_response)
+
+    def add_book(book: Book):
+        result = book_coll.insert_one(book.model_dump(exclude_none=True))
+        book_response = BookWithID(id=str(result.inserted_id), **book.model_dump())
+        return book_response
+
+    def edit_book(book_id: str, book: Book):
+        update_data = {k: v for k,v in book.model_dump(exclude_unset=True).items()}
+        result = book_coll.find_one_and_update({"_id": ObjectId(book_id)},
+                                    {"$set": update_data},
+                                    return_document=True)
+        if result is not None:
+            return Book(**result)
+        
+
+    def delete_book():
+        pass
