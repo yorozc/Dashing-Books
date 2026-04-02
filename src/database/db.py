27ced70161736a70d552f9b59563c18272f30cbf
Yahir Orozco <yorozcolop49@gmail.com>
2026-04-01 22:57:09 -0700

    added error checking for routes

--- src/database/db.py
+++ src/database/db.py
@@ -1,13 +1,15 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import certifi
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
+ca = certifi.where()
 URI = os.getenv("MONGODB_URI")
 DB_NAME = os.getenv("DB_NAME")
 BOOK_COLL = os.getenv("BOOK_COLL")
-client = MongoClient(URI, server_api=ServerApi('1'))
+client = MongoClient(URI, tlsCAFile=ca, server_api=ServerApi('1'))
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
--- src/services/book_service.py
+++ src/services/book_service.py
@@ -19,9 +19,14 @@ class BookService:
         return books
 
     def return_book_with_id(book_id: str) -> BookWithID:
-        book_response = book_coll.find_one({"_id": ObjectId(book_id)})
+        try:
+            book_response = book_coll.find_one({"_id": ObjectId(book_id)})
+        except Exception as e:
+            raise HTTPException(status_code=400, detail="Not a valid ID")
+        
         if book_response is None: 
             raise HTTPException(status_code=404, detail="Book not found")
+        
         book_response["id"] = str(book_response["_id"])
         del book_response["_id"]
         return BookWithID(**book_response)
@@ -32,16 +37,23 @@ class BookService:
         return book_response
 
     def edit_book(book_id: str, book: Book):
-        update_data = {k: v for k,v in book.model_dump(exclude_unset=True).items()}
-        result = book_coll.find_one_and_update({"_id": ObjectId(book_id)},
-                                    {"$set": update_data},
-                                    return_document=True)
+        try:
+            update_data = {k: v for k,v in book.model_dump(exclude_unset=True).items()}
+            result = book_coll.find_one_and_update({"_id": ObjectId(book_id)},
+                                        {"$set": update_data},
+                                        return_document=True)
+        except Exception as e:
+            raise HTTPException(status_code=400, detail="Not a valid ID")
+        
         if result is not None:
             return Book(**result)
         raise HTTPException(status_code=404, detail="Book not found")
         
     def delete_book(book_id: str):
-        del_result = book_coll.find_one_and_delete({"_id": ObjectId(book_id)})
+        try:
+            del_result = book_coll.find_one_and_delete({"_id": ObjectId(book_id)})
+        except Exception as e:
+            raise HTTPException(status_code=400, detail="Not a valid ID")
 
         if del_result is not None:
             return Book(**del_result)
