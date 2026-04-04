8941d8c42c4f4063cae1779e2980d2958c8a1da3
Yahir Orozco <yorozcolop49@gmail.com>
2026-04-03 23:13:52 -0700

    successfully click the title of the book and redirects to specific page

--- src/routes/books.py
+++ src/routes/books.py
@@ -1,5 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, HTTPException
+from services.book_service import BookService
 
 books = APIRouter()
 
 # routes to display books
+
+@books.get("/books/{book_id}", include_in_schema=False)
+async def book(request: Request, book_id: str):
+    templates = request.app.state.templates
+    book = BookService.return_book_with_id(book_id)
+    if book:
+        return templates.TemplateResponse(request, "book.html",
+                                          {"book": book})
+    
+    raise HTTPException(status_code=404, detail="Book not found!")
